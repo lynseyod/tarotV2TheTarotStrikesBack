a79979a43c815d87894c47d11e4b85894c30d064
lynseyod <lyns.odonnell@gmail.com>
2020-01-09 21:16:36 -0500

    it's ugly but IT WORKS :boom::fire::nail_care:

---
+++ script.js
@@ -0,0 +1,49 @@
+const theTarotStrikesBack = {};
+
+// api call for 78 random tarot cards
+// returns an array of the whole deck in random order
+// thank you tarot API for not making me write this function.
+theTarotStrikesBack.setCards = function() {
+  $.ajax({
+    url: `http://proxy.hackeryou.com`,
+    method: `GET`,
+    dataType: `json`,
+    data: {
+      reqUrl: `https://rws-cards-api.herokuapp.com/api/v1/cards/random`,
+      params: {
+        n: 78
+      }
+    }
+  }).then((response) => {
+    response.cards.forEach((card, index) => {
+      console.log(card)
+      $(`.card-container:nth-of-type(${index+1})`)
+      .append(`<h2>${card.name}</h2>
+        <p>${card.desc}</p>
+        <p>${card.meaning_up}</p>`)
+    })
+  })
+}
+
+theTarotStrikesBack.cardSelect = function() {
+  $('.card-container').on("click", function() {
+    if ($('.the-past').length === 0) {
+      $(this).addClass('the-past');
+    } else if ($('.the-present').length === 0) {
+      $(this).addClass('the-present');
+    } else if ($('.the-future').length === 0) {
+      $(this).addClass('the-future');
+    }
+  })
+}
+
+
+
+theTarotStrikesBack.init = function() {
+  theTarotStrikesBack.setCards();
+  theTarotStrikesBack.cardSelect();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  theTarotStrikesBack.init();
+})
