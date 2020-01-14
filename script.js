c97ae403faf4fab03095ca2a4c7d701afe0b3d88
lynseyod <lyns.odonnell@gmail.com>
2020-01-13 23:49:41 -0500

    OH DAMN we got cards displaying!! :crystal_ball::tada:

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 // STRETCH GOALS
-  // keep the card array
-  // reasign the card to the past, present, future when clicked
-  // display past, present, future cards when future is selected
+  // randomize upright and reversed cards
+  // 'shuffle' function
+    // DO IT WITHOUT PINGING THE API AGAIN?
 //THOUGHTS
   // have the array of cards
   // on click, push that card to past/present/future etc
+
+  //have card array:
+  // [{selection: 'the past'}, {selection: 'the present'}, {selection: 'the future'}]
+  // for (i = 0; i <= 2; ++i) {
+    // cardArray[i].card = whateverTheCardIs
+  // }
 
 const theTarotStrikesBack = {};
 
 // empty array of cards
 // helps 'set' past, present, future cards based on clicks
-theTarotStrikesBack.cardArray = [];
+theTarotStrikesBack.cardArray = [
+  {
+    position: 'the-past'
+  },
+  {
+    position: 'the-present'
+  },
+  {
+    position: 'the-future'
+  },
+];
 
 // returns an array of the whole deck in random order
 // thank you tarot API for not making me write a randomize function
-theTarotStrikesBack.setCards = function() {
+theTarotStrikesBack.setCards = () => {
   $.ajax({
     url: `http://proxy.hackeryou.com`,
     method: `GET`,
@@ -28,6 +44,7 @@ theTarotStrikesBack.setCards = function() {
   }).then((response) => {
     // setting the cards in the deck
     // I want to do this BEFORE people click so they can feel out the deck
+    // I'm aware it's fake but... I WANT TO BELIEVE
     response.cards.forEach((card, index) => {
       $(`.card-container:nth-of-type(${index+1})`)
       .append(`<h2>${card.name}</h2>
@@ -37,25 +54,38 @@ theTarotStrikesBack.setCards = function() {
   })
 }
 
-// event listener to select cards from the deck
-theTarotStrikesBack.cardSelect = function() {
-  $('.card-container').on("click", function() {
-    if (theTarotStrikesBack.cardArray.length < 3) { // only want to select 3 cards!
-      theTarotStrikesBack.cardArray.push($(this));
-      $(this).addClass("clickedIt"); // to remove them from the stack
-    } else { // if we've selected 3 cards, we show the results!
-      // theTarotStrikesBack.displayCards();
-      console.log("STOP CLICKING")
-      console.log(theTarotStrikesBack.cardArray)
-    }
+theTarotStrikesBack.displayCards = () => {
+  theTarotStrikesBack.cardArray.forEach((card) => {
+    $('footer').append(
+      `<div class=${card.position}>
+        ${card.cardHTML}
+      </div>`
+    )
   })
 }
 
-theTarotStrikesBack.init = function() {
+// event listener to select cards from the deck
+theTarotStrikesBack.cardSelect = () => {
+  let clicks = 0;
+  $('.card-container').on("click", function() {
+    if (clicks <= 2) { // only want to select 3 cards!
+      theTarotStrikesBack.cardArray[clicks].cardHTML = ($(this)[0].innerHTML);
+      $(this).addClass("clickedIt"); //remove them from the stack
+      if (clicks === 2) {
+        $('.card-container').unbind("click");
+        console.log(theTarotStrikesBack.cardArray)
+        theTarotStrikesBack.displayCards();
+      }
+    }
+    clicks++;
+  })
+}
+
+theTarotStrikesBack.init = () => {
   theTarotStrikesBack.setCards();
   theTarotStrikesBack.cardSelect();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   theTarotStrikesBack.init();
 })
